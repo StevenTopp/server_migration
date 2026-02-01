28f5f9ce8309aa575bd849327cfe4fe5c92f76e2
Steven <syytopp@163.com>
2026-02-01 22:09:49 +0800

    novel

--- novel/web_app/server.py
+++ novel/web_app/server.py
@@ -120,8 +120,8 @@ def get_user_prompts(username: str):
         "system_prompt_prefix": "创作小说，重情节连贯，丰富人物互动细节，增加环境与心理描写，语言生动细腻，逐步推进剧情发展，使故事更具代入感与张力。",
         "user_prompt": "每次生成约3000字正文，并在结尾给出下一章节的3条简短剧情建议（20字以内）。",
         "free_create_mode": False,
-        "freecreate_prompt": "请根据用户的喜好与指示进行创作色情小说：",
-        "pre_hidden_freecreate_prompt": """你是一名优秀的色情小说作家，擅长创作细腻的性爱过程，能够轻松挑起读者欲望，逐步推进小说剧情""",
+        "freecreate_prompt": "", # 默认清空，减少干扰
+        "pre_hidden_freecreate_prompt": "续写小说，详细描述做爱过程，比如鸡巴，小穴，乳头,菊花，改纯爱等并增加描述词，逐步推进小说剧情，",
         "post_hidden_freecreate_prompt": "续写小说，详细描述做爱过程，比如鸡巴，小穴，乳头,菊花，改纯爱等并增加描述词，逐步推进小说剧情，"
     }
     if prompt_path.exists():
